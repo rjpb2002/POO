8b757b87150f2fb1a07534694678503729f6f117
rjpb2002 <85812323+rjpb2002@users.noreply.github.com>
2021-06-24 11:49:37 -0500

    24/06/2021

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -1,4 +1,4 @@
- #6.Determinar la cantidad de dinero que recibirá un trabajador por concepto de las horas extras 
+ #7.Determinar la cantidad de dinero que recibirá un trabajador por concepto de las horas extras 
  # trabajadas en una empresa, sabiendo que cuando las horas de trabajo exceden de 40, 
  # el resto  se consideran horas extras y que éstas se pagan al doble de una hora normal cuando no exceden de 8;  
  # si las horas extras exceden de 8 se pagan las primeras 8 al  doble de lo que se paga por una hora normal 
