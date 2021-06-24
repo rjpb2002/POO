246d92ebcafc8315663fe0ed855f88446cc98193
rjpb2002 <85812323+rjpb2002@users.noreply.github.com>
2021-06-24 11:48:40 -0500

    24/06/2021

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,4 +1,4 @@
-#5.-Dado el sueldo de un empleado, encontrar el nuevo sueldo si obtiene un aumento del 10% 
+#6.-Dado el sueldo de un empleado, encontrar el nuevo sueldo si obtiene un aumento del 10% 
 # si su sueldo es inferior a $600, en caso contrario no tendrá aumento.
 
 class empleado:
